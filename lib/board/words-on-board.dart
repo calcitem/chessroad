5efa22214419ffb65a8ced6b6f34760935a98b42
Calcitem <calcitem@outlook.com>
2020-10-31 23:48:56 +0800

    Remove Chuhe/Hanjie etc

--- lib/board/words-on-board.dart
+++ lib/board/words-on-board.dart
@@ -7,40 +7,20 @@ class WordsOnBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //
-    final blackColumns = '１２３４５６７８９', redColumns = '九八七六五四三二一';
     final bChildren = <Widget>[], rChildren = <Widget>[];
 
-    final digitsStyle = TextStyle(fontSize: DigitsFontSize);
-    final rivierTipsStyle = TextStyle(fontSize: 28.0);
-
     for (var i = 0; i < 7; i++) {
-      //
-      bChildren.add(Text(blackColumns[i], style: digitsStyle));
-      rChildren.add(Text(redColumns[i], style: digitsStyle));
-
       if (i < 8) {
         bChildren.add(Expanded(child: SizedBox()));
         rChildren.add(Expanded(child: SizedBox()));
       }
     }
 
-    final riverTips = Row(
-      children: <Widget>[
-        Expanded(child: SizedBox()),
-        Text('楚河', style: rivierTipsStyle),
-        Expanded(child: SizedBox(), flex: 2),
-        Text('汉界', style: rivierTipsStyle),
-        Expanded(child: SizedBox()),
-      ],
-    );
-
     return DefaultTextStyle(
       child: Column(
         children: <Widget>[
           Row(children: bChildren),
           Expanded(child: SizedBox()),
-          riverTips,
           Expanded(child: SizedBox()),
           Row(children: rChildren),
         ],
